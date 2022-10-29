526bc9a1f9a67f2e33aa054f3834110d1253df99
AnnaMateichik <anna.mateichik@yandex.ru>
2022-10-29 14:05:31 +0300

    home work 11 Anna Mateichik

---
+++ app.js
@@ -0,0 +1,16 @@
+function sumDigits(number) {
+    //TODO
+    //function should return sum of a given number digits
+     if (number < 0){
+        number *= -1;
+     }
+    var sum = 0, rest;
+    while (number) {
+        rest = number % 10;
+        number = (number - rest) / 10;
+        sum += rest;
+    }
+    return sum;
+}
+//Example
+console.log(sumDigits(123)); //should be printed out 6
