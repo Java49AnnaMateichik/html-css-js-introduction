ca99501f3ab05009bd29d178a4024a77bd842353
AnnaMateichik <anna.mateichik@yandex.ru>
2022-12-03 20:53:42 +0200

    HW21

--- src/form.js
+++ src/form.js
@@ -1,4 +1,9 @@
 const inputElements = document.querySelectorAll(".form-class [name]");
+const errorEllement =document.querySelector(".error");
+const currentYear = new Date().getFullYear();
+const minYear = 1950;
+let timerID =-1;
+
 function onSubmit(event) {
     event.preventDefault();
     console.log("submitted");
@@ -13,7 +18,26 @@ function onSubmit(event) {
 function onChange(event) {
     if (event.target.name == "salary") {
         if(+event.target.value < 1000 || +event.target.value > 40000){
-            event.target.value=''
+           error(event,
+            `salary need to be more than 1000 and less than 40000. salary= ${+event.target.value}`);
         }
     }
+
+    if (event.target.name == "birthDate") {
+        const arDate = (event.target.value).split('-');
+        const selectedYear = +arDate[0];
+        if( selectedYear < minYear || selectedYear > currentYear){
+            error(event,
+                `year has to be more then ${minYear } and less than ${currentYear}. selected year = ${selectedYear}`);
+        }
+    }
+    
+}
+function error(event, message) {
+    event.target.value ='';
+    errorEllement.innerHTML = "ERROR: the value of" + message;
+    timerID = setTimeout(()=> {
+        errorEllement.innerHTML = '';
+        
+    },5000)
 }
